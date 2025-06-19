453b12ba1630c624a010f977b9bd633df0992756
Fatih Eker <fatiheker97@gmail.com>
2025-06-19 15:13:33 +0300

    İnitial Commit
    +tailwind
    +toastify
    +axios

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div class="w-full h-screen bg-red-500">
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
